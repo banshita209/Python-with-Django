667b9141363a167df73274378ca23f53aa744a40
Banshita Gangwar <banshitagangwar2000@gmail.com>
2021-06-14 12:22:15 +0530

    adding report and some ss

--- Day 10/djangoIntern/mark1/migrations/0001_initial.py
+++ Day 10/djangoIntern/mark1/migrations/0001_initial.py
@@ -2,14 +2,10 @@
 
 from django.db import migrations, models
 
-
 class Migration(migrations.Migration):
-
     initial = True
-
     dependencies = [
     ]
-
     operations = [
         migrations.CreateModel(
             name='Student',
